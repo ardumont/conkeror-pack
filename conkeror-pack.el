363696a08095ad39fea46eb51ab4cfe46e2ff4d0
Antoine R. Dumont (@ardumont) <antoine.romain.dumont@gmail.com>
2018-11-06 02:28:22 +0100

    conkeror-pack: Simplify pack

--- conkeror-pack.el
+++ conkeror-pack.el
@@ -9,25 +9,23 @@
 
 ;;; Code:
 
-(use-package js)
-(use-package conkeror-minor-mode
-  :config
-  ;; for all javascript files
-  ;; (add-hook 'js-mode-hook 'conkeror-minor-mode)
+(require 'js)
+(require 'conkeror-minor-mode)
+;; for all javascript files
+;; (add-hook 'js-mode-hook 'conkeror-minor-mode)
 
-  ;; If you want it only on some files, you can have it activate only on your .conkerorrc file:
-  ;; (add-hook 'js-mode-hook (lambda ()
-  ;;                           (when (string= ".conkerorrc" (buffer-name))
-  ;;                             (conkeror-minor-mode 1))))
+;; If you want it only on some files, you can have it activate only on your .conkerorrc file:
+;; (add-hook 'js-mode-hook (lambda ()
+;;                           (when (string= ".conkerorrc" (buffer-name))
+;;                             (conkeror-minor-mode 1))))
 
-  ;; alternatively, only on files with "conkeror" somewhere in the path:
-  (add-hook 'js-mode-hook (lambda ()
-                            (when (string-match "conkeror" (buffer-file-name))
-                              (conkeror-minor-mode 1)))))
+;; alternatively, only on files with "conkeror" somewhere in the path:
+(add-hook 'js-mode-hook (lambda ()
+			  (when (string-match "conkeror" (buffer-file-name))
+			    (conkeror-minor-mode 1))))
 
-(use-package moz
-  :config
-  (add-hook 'javascript-mode-hook 'moz-minor-mode))
+(require 'moz)
+(add-hook 'javascript-mode-hook 'moz-minor-mode)
 
 (provide 'conkeror-pack)
 ;;; conkeror-pack.el ends here
